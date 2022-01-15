9c93a93a31d119c887b7cb558f645828e576c212
anrahulananth <rahul.anantharama@gmail.com>
2022-01-15 20:06:11 +0530

    CDN Check

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -34,7 +34,7 @@ export default class NewDocument extends Document {
             <Html lang="en">
                 <Head>
                     <CommonHead />
-                    <link href='http://fonts.cdnfonts.com/css/poppins?styles=20394,20389,20390,20391,20392,20393,20395,20396,20383,20384,20385,20386,20381,20382&display=swap' rel='stylesheet' />
+                    <link href='https://fonts.cdnfonts.com/css/poppins?styles=20394,20389,20390,20391,20392,20393,20395,20396,20383,20384,20385,20386,20381,20382&display=swap' rel='stylesheet' />
                 </Head>
                 <body>
                     <Main />
