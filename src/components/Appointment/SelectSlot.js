ed273bfd3ff7ccb0b4213842c894d7bfeb70cfea
anrahulananth <rahul.anantharama@gmail.com>
2022-02-06 11:43:05 +0530

    Added Slot Time Handler

--- src/components/Appointment/SelectSlot.js
+++ src/components/Appointment/SelectSlot.js
@@ -78,6 +78,7 @@ const SelectSlot = ({ appointmentData, handleSlotData }) => {
                                 {
                                     slotsData[timeOfDay].map((slotTime) => (
                                         <button
+                                          onClick={() => handleSlotData({ slot: slotTime })}
                                           key={slotTime}
                                           className={classNames(
                                           "border border-background8 rounded-md text-text2 py-2 px-4 m-2 hover:border-primary1",
