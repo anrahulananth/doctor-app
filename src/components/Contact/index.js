3b4f3ad4a9426d74cb0d42648c09f350be46bc25
anrahulananth <rahul.anantharama@gmail.com>
2022-03-08 23:25:29 +0530

    Google Maps Integration

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,7 +5,7 @@ import classNames from "classnames";
 
 const MapWithMarker = () => {
     return (
-        <div className="w-full h-[320px] md:h-[900px] overflow: hidden;">
+        <div className="relative w-full h-[320px] md:h-[900px] overflow: hidden;">
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.0834777911455!2d77.61211594993453!3d12.966509990814352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc48a1ebb181756e6!2zMTLCsDU3JzU5LjQiTiA3N8KwMzYnNTEuNSJF!5e0!3m2!1sen!2sin!4v1646758902776!5m2!1sen!2sin"
                 className="w-full h-full"
@@ -13,6 +13,7 @@ const MapWithMarker = () => {
                 allowFullScreen=""
                 loading="lazy"
             />
+            <div className="absolute top-0 left-0 h-[38px] w-full bg-background1" />
         </div>
     );
 };
@@ -89,7 +90,7 @@ const ContactUs = ({ isPhone }) => (
 export default function Contact() {
     const { isPhone } = useDetectScreen();
     return (
-        <section id="contact" className="relative overflow-hidden bg-background1 pt-16 sm:pt-20 md:pt-24">
+        <section id="contact" className="relative overflow-hidden bg-background1 pt-14 sm:pt-18 md:pt-22">
             {
                 !isPhone ? (
                     <div className="relative w-full h-full">
@@ -103,7 +104,6 @@ export default function Contact() {
                     </div>
                 )
             }
-
         </section>
     );
 }
